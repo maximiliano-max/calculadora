9be9c89c356006fe8fabc570374f266602e5d3e8
maximiliano <maximilianodonofio9@gmail.com>
2023-08-03 12:13:00 -0300

    first commit

--- src/componentes/boton.js
+++ src/componentes/boton.js
@@ -4,7 +4,7 @@ import '../estilo/Boton.css'
 function Boton(props) {
 
     const esOperador = valor => {
-        return isNaN(valor) && (valor != '.') && (valor != '=');
+        return isNaN(valor)
     }
 
   return (
